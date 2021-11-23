d4cbb3e8b9c31f476d871cb19ea44c256588cd2d
Emir Avci <emiravc@gmail.com>
2021-11-22 22:00:26 -0700

    Button is properly fixed
    When button is pressed it opens a new website with "/quiz" using the main template.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./Pages/Home/Home";
+import Quiz from "./Pages/Quiz/Quiz";
+import "./App.css";
 function App() {
   return (
     <BrowserRouter>
       <div className="App" style={{ backgroundImage: "url(./circle.jpg)" }}>
         <Header />
-        <Home />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
       <Footer />
     </BrowserRouter>
