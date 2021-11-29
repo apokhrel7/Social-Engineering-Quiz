f7bdeec9b215840d046afe04c4ca2732fa39a914
Anish Pokhrel <anishpokhrel2002@gmail.com>
2021-11-29 03:41:59 -0700

    Added scam and real images

--- src/Pages/Quiz5/Quiz5.jsx
+++ src/Pages/Quiz5/Quiz5.jsx
@@ -1,149 +1,291 @@
-import * as React from 'react';
+/* import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
+import Box from "@mui/material/Box";
+import Backdrop from "@mui/material/Backdrop";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
 import Stack from "@mui/material/Stack";
-import  {Button}  from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import { Container } from "@material-ui/core";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import { createTheme } from '@mui/material/styles';
-import styles from'./Quiz5.css';
-import { ButtonGroup } from '@mui/material';
-import { palette } from '@mui/system';
-import { green } from '@mui/material/colors';
+import styles from'./Quiz5.css'; */
+
+import * as React from "react"
+import Button from "@mui/material/Button";
+import NextQuestion from '../../Pages/Quiz3/NextQuestion';
+import Hint from '../../Pages/Quiz3/Hint';
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import { Container } from "@material-ui/core";
 
 
+
+
+
+
+import { useScoreContext, useUpdateScoreContext } from '../../components/Context/ResultContext';
+
+import {useResponseContext, useUpdateResponseContext} from '../../components/Context/ResultContext';
+
+// Temporary ------------------------------------------------------------
+import { useNavigate } from 'react-router-dom';
+// ----------------------------------------------------------------------
+
+import {
+    AnswerStyles,
+    CorrectStyles,
+    DisableStyles,
+    WrongStyles,
+  } from "../Quiz3/Styling";
+import { HintStyles } from "../Quiz3/Styling";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function Quiz5() {
-    
 
-    const [color, setColor] = React.useState("primary")
+  // Temporary ----------------------------------------------------------
+  const navigate = useNavigate();
+  // --------------------------------------------------------------------
 
-    
+  const [correctFlag, setcorrectFlag] = React.useState(true);
+  const [disableCorrectFlag, setdisableCorrectFlag] = React.useState(true);
+  const [wrongFlag, setWrongFlag] = React.useState(true);
+  const [disableWrongFlag, setdisableWrongFlag] = React.useState(true);
+  const [next, setNext] = React.useState(true);
 
-    function rightAnswer() {
-        setColor("secondary")
-        
-    }
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
+  // used for updating results ------------------------------------------
+  const score = useScoreContext();
+  const updateScore = useUpdateScoreContext();
+
+  const responses = useResponseContext();
+  const updateResponses = useUpdateResponseContext();
+  // --------------------------------------------------------------------
+
+  const handleClickWrong = () => {
+    setWrongFlag(!wrongFlag);
+    setNext(!next);
+    setdisableCorrectFlag(!disableCorrectFlag);
+  };
+
+  const handleClickCorrect = () => {
+    setcorrectFlag(!correctFlag);
+    setNext(!next);
+    setdisableWrongFlag(!disableWrongFlag);
+    updateScore(Number(score) + 1);
+  };
+
+  const handleNext = () => {
+    updateResponses(Number(responses) + 1);
+    navigate("/Quiz5");
+  };
 
   return (
     <Container>
       <Stack
-        backgroundColor= "white"
-        boxShadow= "4px 4px lightblue"
-        marginLeft = "5%"
-        marginRight = "5%"
-        borderRadius= "7px"
-        padding= "2px"
-        fontSize= "0.875rem"
-        boxSizing= "border-box"
-        letterSpacing= "0.01071em"
-        borderWidth= "1"
-        lineHeight= "1.43"
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        spacing={3}
-      >
-        <h1 className= {styles.Q5title}>Which one is the scam?</h1>
-      </Stack>
-    
-    
-      <Stack
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        spacing={3}
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={3}
       >
         <Stack
-          direction="row"
+          backgroundColor= "white"
+          boxShadow= "4px 4px lightblue"
+          borderRadius= "7px"
+          padding= "24px"
+          fontSize= "0.875rem"
+          boxSizing= "border-box"
+          letterSpacing= "0.01071em"
+          borderWidth= "1"
+          lineHeight= "1.43"
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          spacing={3}
+        >
+          <h1 >Which one is the fraudulent UofC login page?</h1>
+        </Stack>
+        <Stack
+          direction="column"
           justifyContent="center"
           alignItems="center"
           spacing={2}
         >
-          <Card
-            sx={{
-              marginTop: 1,
-              maxWidth: 400,
-              borderRadius: 6,
-              padding: "10px",
-              boxShadow: "4px 4px lightblue",
-            }}
+          <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={12}
+            >
+              <Card
+                sx={{
+                  marginTop: 1,
+                  maxWidth: 600,
+                  borderRadius: "24px",
+                  padding: "10px",
+                  boxShadow: "4px 4px lightblue",
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  padding="2.5rem 5.0rem"
+                  height="20%"
+                  width="100%"
+                  overflow="hidden"
+                  backgroundSize="cover"
+                  src="/Quiz5_scam.png"
+                  alt=""
+                />
+              </Card>
+              <Card
+                sx={{
+                  marginTop: 1,
+                  maxWidth: 600,
+                  borderRadius: "24px",
+                  borderRadius: 6,
+                  padding: "10px",
+                  boxShadow: "4px 4px lightblue",
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  padding="2.5rem 5.0rem"
+                  height="20%"
+                  width="100%"
+                  overflow="hidden"
+                  src="/Quiz5_real.png"
+                  alt=""
+                />
+              </Card>
+            </Stack>
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={60}
           >
-            <CardMedia
-              component="img"
-              padding="2.5rem 5.0rem"
-              height="20%"
-              src="/fake1.png"
-              alt=""
-            />
-          </Card>
-          <Card
-            sx={{
-              marginTop: 1,
-              maxWidth: 400,
-              borderRadius: 6,
-              padding: "10px",
-              boxShadow: "4px 4px lightblue",
-            }}
+            {disableCorrectFlag ? (
+              !correctFlag ? (
+                <Button sx={CorrectStyles} variant="contained" size="medium">
+                <Typography style={{ fontWeight: 600 }}>Correct!</Typography>
+              </Button>
+
+              ) : (
+                <Button
+                sx={AnswerStyles}
+                onClick={handleClickCorrect}
+                variant="contained"
+                size="medium"
+              >
+                <Typography style={{ fontWeight: 600 }}>A</Typography>
+              </Button>
+                )
+              ) : (
+                <Button sx={DisableStyles} variant="contained" size="medium">
+                <Typography style={{ fontWeight: 600 }}>A</Typography>
+              </Button>
+
+              )}
+            {disableWrongFlag ? (
+              !wrongFlag ? (
+                <Button sx={WrongStyles} variant="contained" size="medium">
+                <Typography style={{ fontWeight: 600 }}>Wrong!</Typography>
+              </Button>
+              ) : (
+                <Button
+                    sx={AnswerStyles}
+                    onClick={handleClickWrong}
+                    variant="contained"
+                    size="medium"
+                >
+                    <Typography style={{ fontWeight: 600 }}>B</Typography>
+                </Button>
+                )
+              ) : (
+                <Button sx={DisableStyles} variant="contained" size="medium">
+                <Typography style={{ fontWeight: 600 }}>B</Typography>
+              </Button>
+              )}
+          </Stack>
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={10}
           >
-            <CardMedia
-              component="img"
-              padding="2.5rem 5.0rem"
-              height="20%"
-              src="/real1.png"
-              alt=""
-            />
-          </Card>
-        </Stack>
-        <Stack
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          spacing={40}
-        >
-          <Button
-            variant="contained"
-            color={color}
-            style={{ height: 50 }}
-            style={{ width: 200 }}
-            onClick = {() => rightAnswer()}
-          >
-          A
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ height: 50 }}
-            style={{ width: 200 }}
-          >
-          B
-          </Button>
-        </Stack>
-        <Stack
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          spacing={10}
-        >
-          <Button
-            variant="contained"
-            color="secondary"
-            style={{ height: 50 }}
-            style={{ width: 150 }}
-          >
-          hint
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ height: 50 }}
-            style={{ width: 150 }}
-          >
-          Next Question
-          </Button>
+            <Button
+            sx={HintStyles}
+                onClick={handleOpen}
+                variant="contained"
+                size="medium"
+            >
+            Hint
+             </Button>
+           
+            <Modal
+              open={open}
+              onClose={handleClose}
+              closeAfterTransition
+              BackdropComponent={Backdrop}
+              BackdropProps={{
+                timeout: 500,
+              }}
+            >
+              <Fade in={open}>
+                <Box sx={style}>
+                  <h2>Hint</h2>
+                  <p>
+                    {''}
+                    Both images are very similar, so don't fall for looks!
+                    Instead, look carefully at the image URL.
+                    {' '}
+                    Notice how the left image has "cas.ucalgary.net" and
+                    some additional suspicious URL content. 
+              
+                    Therefore, the left image is a fraudulent login page!
+                  </p>
+                </Box>
+              </Fade>
+            </Modal>
+            {!next ? (
+                <NextQuestion />
+                ) : (
+                    <Button
+                    style={{
+                        height: 50,
+                        width: 150
+                    }}
+                    disabled
+                    variant="contained"
+                    size="medium"
+                  >
+                    Next Question
+                  </Button>
+                )}
+
+
+          </Stack>
         </Stack>
       </Stack>
     </Container>
