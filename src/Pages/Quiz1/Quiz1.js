873a7cfd525e3e9c9c9acb01d5055e256f4553fa
areza244 <94305849+areza244@users.noreply.github.com>
2021-11-29 18:29:15 +0400

    d

--- src/Pages/Quiz1/Quiz1.js
+++ src/Pages/Quiz1/Quiz1.js
@@ -1,24 +1,40 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Backdrop from "@mui/material/Backdrop";
 import Modal from "@mui/material/Modal";
 import Fade from "@mui/material/Fade";
 import Stack from "@mui/material/Stack";
-import { Button } from "@material-ui/core";
+import Button from "@mui/material/Button";
 import { Container } from "@material-ui/core";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
-import { createTheme } from '@mui/material/styles';
-import styles from'./Quiz1.module.css';
+import { createTheme } from "@mui/material/styles";
+import styles from "./Quiz1.module.css";
+import Typography from "@mui/material/Typography";
 
-import { useScoreContext, useUpdateScoreContext } from '../../components/Context/ResultContext';
 
-import {useResponseContext, useUpdateResponseContext} from '../../components/Context/ResultContext';
+import {
+  useScoreContext,
+  useUpdateScoreContext,
+} from "../../components/Context/ResultContext";
+
+import {
+  useResponseContext,
+  useUpdateResponseContext,
+} from "../../components/Context/ResultContext";
 
 // Temporary ------------------------------------------------------------
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 // ----------------------------------------------------------------------
+import {
+  AnswerStyles,
+  CorrectStyles,
+  DisableStyles,
+  WrongStyles,
+  HintStyles,
+  NextQuestionStyles,
+} from "./Styling1";
 
 const style = {
   position: "absolute",
@@ -33,7 +49,6 @@ const style = {
 };
 
 export default function Quiz1() {
-
   // Temporary ----------------------------------------------------------
   const navigate = useNavigate();
   // --------------------------------------------------------------------
@@ -77,27 +92,27 @@ export default function Quiz1() {
   return (
     <Container>
       <Stack
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      spacing={3}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={3}
       >
         <Stack
-          backgroundColor= "white"
-          boxShadow= "4px 4px lightblue"
-          borderRadius= "7px"
-          padding= "24px"
-          fontSize= "0.875rem"
-          boxSizing= "border-box"
-          letterSpacing= "0.01071em"
-          borderWidth= "1"
-          lineHeight= "1.43"
+          backgroundColor="white"
+          boxShadow="4px 4px lightblue"
+          borderRadius="7px"
+          padding="24px"
+          fontSize="0.875rem"
+          boxSizing="border-box"
+          letterSpacing="0.01071em"
+          borderWidth="1"
+          lineHeight="1.43"
           direction="column"
           justifyContent="center"
           alignItems="center"
           spacing={3}
         >
-          <h1 className= {styles.Q1title}>Which one is the scam?</h1>
+          <h1 className={styles.Q1title}>Which one is the scam?</h1>
         </Stack>
         <Stack
           direction="column"
@@ -106,52 +121,52 @@ export default function Quiz1() {
           spacing={2}
         >
           <Stack
-              direction="row"
-              justifyContent="center"
-              alignItems="center"
-              spacing={12}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={25}
+          >
+            <Card
+              sx={{
+                marginTop: 1,
+                maxWidth: 400,
+                borderRadius: "24px",
+                padding: "10px",
+                boxShadow: "4px 4px lightblue",
+              }}
             >
-              <Card
-                sx={{
-                  marginTop: 1,
-                  maxWidth: 400,
-                  borderRadius: "24px",
-                  padding: "10px",
-                  boxShadow: "4px 4px lightblue",
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  padding="2.5rem 5.0rem"
-                  height="20%"
-                  width="100%"
-                  overflow="hidden"
-                  backgroundSize="cover"
-                  src="/fake1.1.png"
-                  alt=""
-                />
-              </Card>
-              <Card
-                sx={{
-                  marginTop: 1,
-                  maxWidth: 400,
-                  borderRadius: "24px",
-                  borderRadius: 6,
-                  padding: "10px",
-                  boxShadow: "4px 4px lightblue",
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  padding="2.5rem 5.0rem"
-                  height="20%"
-                  width="100%"
-                  overflow="hidden"
-                  src="/real1.1.png"
-                  alt=""
-                />
-              </Card>
-            </Stack>
+              <CardMedia
+                component="img"
+                padding="2.5rem 5.0rem"
+                height="20%"
+                width="100%"
+                overflow="hidden"
+                backgroundSize="cover"
+                src="/fake1.1.png"
+                alt=""
+              />
+            </Card>
+            <Card
+              sx={{
+                marginTop: 1,
+                maxWidth: 400,
+                borderRadius: "24px",
+                borderRadius: 6,
+                padding: "10px",
+                boxShadow: "4px 4px lightblue",
+              }}
+            >
+              <CardMedia
+                component="img"
+                padding="2.5rem 5.0rem"
+                height="20%"
+                width="100%"
+                overflow="hidden"
+                src="/real1.1.png"
+                alt=""
+              />
+            </Card>
+          </Stack>
           <Stack
             direction="row"
             justifyContent="center"
@@ -160,77 +175,44 @@ export default function Quiz1() {
           >
             {disableCorrectFlag ? (
               !correctFlag ? (
-                <Button
-                  variant="contained"
-                  color="primary"
-                  size="medium"
-                  style={{ height: 50 }}
-                  style={{ width: 200 }}
-                >
-                  Correct!
+                <Button sx={CorrectStyles} variant="contained" size="medium">
+                  <Typography style={{ fontWeight: 600 }}>Correct!</Typography>
                 </Button>
               ) : (
                 <Button
+                  sx={AnswerStyles}
                   onClick={handleClickCorrect}
                   variant="contained"
-                  color="primary"
                   size="medium"
-                  fontWeight="600"
-                  style={{ height: 50 }}
-                  style={{ width: 200 }}
                 >
-                  A
+                  <Typography style={{ fontWeight: 600 }}>A</Typography>
                 </Button>
-                )
-              ) : (
-                <Button
-                  variant="contained"
-                  color="primary"
-                  size="medium"
-                  fontWeight="600"
-                  style={{ height: 50 }}
-                  style={{ width: 200 }}
-                >
-                  A
-                </Button>
-              )}
+              )
+            ) : (
+              <Button sx={DisableStyles} variant="contained" size="medium">
+                <Typography style={{ fontWeight: 600 }}>A</Typography>
+              </Button>
+            )}
             {disableWrongFlag ? (
               !wrongFlag ? (
-                <Button
-                  variant="contained"
-                  color="primary"
-                  size="medium"
-                  fontWeight="600"
-                  style={{ height: 50 }}
-                  style={{ width: 200 }}
-                >
-                  Wrong!
+                <Button sx={WrongStyles} variant="contained" size="medium">
+                  <Typography style={{ fontWeight: 600 }}>Wrong!</Typography>
                 </Button>
               ) : (
                 <Button
+                  sx={AnswerStyles}
                   variant="contained"
-                  color="primary"
                   size="medium"
-                  fontWeight="600"
                   onClick={handleClickWrong}
-                  style={{ height: 50 }}
-                  style={{ width: 200 }}
                 >
-                  B
+                  <Typography style={{ fontWeight: 600 }}>B</Typography>
                 </Button>
-                )
-              ) : (
-                <Button
-                  variant="contained"
-                  color="primary"
-                  size="medium"
-                  fontWeight="600"
-                  style={{ height: 50 }}
-                  style={{ width: 200 }}
-                >
-                B
-                </Button>
-              )}
+              )
+            ) : (
+              <Button sx={DisableStyles} variant="contained" size="medium">
+                <Typography style={{ fontWeight: 600 }}>B</Typography>
+              </Button>
+            )}
           </Stack>
           <Stack
             direction="row"
@@ -238,14 +220,8 @@ export default function Quiz1() {
             alignItems="center"
             spacing={10}
           >
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={handleOpen}
-              style={{ height: 50 }}
-              style={{ width: 150 }}
-            >
-            hint
+            <Button sx={HintStyles} variant="contained" onClick={handleOpen}>
+              Hint
             </Button>
             <Modal
               open={open}
@@ -260,34 +236,29 @@ export default function Quiz1() {
                 <Box sx={style}>
                   <h2>Hint: </h2>
                   <p>
-                    {' '}
-                    Look closely at the text,
-                    is there any grammatical errors?
+                    {" "}
+                    Look closely at the text, is there any grammatical errors?
                   </p>
                 </Box>
               </Fade>
             </Modal>
             {!next ? (
-              <Button
-                variant="contained"
-                color="primary"
-                size="medium"
-                onClick={handleNext}
-                style={{ height: 50 }}
-                style={{ width: 150 }}
-              >
+              <Button sx={NextQuestionStyles} variant="contained" size="medium">
                 Next Question
               </Button>
             ) : (
               <Button
+                style={{
+                  maxWidth: "250px",
+                  maxHeight: "40px",
+                  minWidth: "250px",
+                  minHeight: "40px",
+                }}
                 disabled
                 variant="contained"
-                color="primary"
                 size="medium"
-                style={{ height: 50 }}
-                style={{ width: 150 }}
               >
-              Next Question
+                Next Question
               </Button>
             )}
           </Stack>
