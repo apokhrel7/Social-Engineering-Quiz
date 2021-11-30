f8c98182acb89d3951c5d11a98a99480886b1b10
RobertZ225 <77633975+RobertZ225@users.noreply.github.com>
2021-11-30 10:31:26 -0700

    Routed in Quiz 6 and 7

--- src/components/QuestionBank/QuestionBank.js
+++ src/components/QuestionBank/QuestionBank.js
@@ -1,12 +1,16 @@
 import Quiz1 from "../../Pages/Quiz1/Quiz1";
 import Quiz3 from "../../Pages/Quiz3/Quiz3";
 import Quiz5 from "../../Pages/Quiz5/Quiz5";
+import Quiz6 from "../../Pages/Quiz6/Quiz6";
+import Quiz7 from "../../Pages/Quiz7/Quiz7";
 import Result from "../../Pages/Result/Result";
 
  export const questionBank = {
     "0" : <Quiz1/>,
     "1" : <Quiz3/>,
     "2" : <Quiz5/>,
-    "3" : <Result/>
+    "3" : <Quiz6/>,
+    "4" : <Quiz7/>,
+    "5" : <Result/>
 };
 
