06bb2dab2989d078d5eabbe77a97de8a1290c486
RobertZ225 <77633975+RobertZ225@users.noreply.github.com>
2021-11-30 22:14:58 -0700

    added underline to my hint

--- src/Pages/Quiz2/Hint.js
+++ src/Pages/Quiz2/Hint.js
@@ -46,9 +46,10 @@ export default function Hint() {
         }}
       >
         <Fade in={open}>
-          <Box sx={style}>
+          <Box sx={style}> 
             <Typography id="transition-modal-title" variant="h6" component="h2">
-              Hint:
+              <h2><u>Hint:</u></h2>
+              
             </Typography>
             <Typography id="transition-modal-description" sx={{ mt: 2 }}>
               Always look at the URL for any subtle difference{" "}
