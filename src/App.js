cc97e0d3bfa49187a1d26f6ae9d29c77e06fc2a4
RobertZ225 <77633975+RobertZ225@users.noreply.github.com>
2021-12-01 22:04:10 -0700

    removed reduntant imports

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./Pages/Home/Home";
 import Quiz from "./Pages/Quiz/Quiz";
-import Quiz1 from "./Pages/Quiz1/Quiz1";
-import Quiz3 from "./Pages/Quiz3/Quiz3";
-import Quiz5 from "./Pages/Quiz5/Quiz5";
-import Quiz6 from "./Pages/Quiz6/Quiz6";
-import Quiz7 from "./Pages/Quiz7/Quiz7";
-import Result from "./Pages/Result/Result";
+
 import "./App.css";
 import {ResultProvider} from './components/Context/ResultContext';
 function App() {
